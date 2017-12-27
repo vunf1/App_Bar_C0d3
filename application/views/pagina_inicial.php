22b2e69fc7673e58637e849ab6dd022648e6c42e
vunf1 <jokass.workplace@gmail.com>
2017-12-27 03:02:51 +0000

    Google drive Link expired so put on local to avoid is type of errors

--- application/views/pagina_inicial.php
+++ application/views/pagina_inicial.php
@@ -22,7 +22,7 @@
         <div class="logo"></div>
           <br><br><br><br>
         <div class="botao1 centered" >
-          <a class="text">Entra nesta Galaxia</a>
+          <a class="texto">Entra nesta Galaxia</a>
         </div>
     </div>
 </div>
