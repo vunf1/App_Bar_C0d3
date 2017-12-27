952f8cd3513f997d81a457929c31fea8ecf58832
vunf1 <epg.joaomaia@gmail.com>
2017-12-27 00:06:00 +0000

    Criacao da pagina inicial e organizacao das configuracoes Codeigniter para permitir acesso-htaccess

---
+++ application/controllers/PageInicial.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class PageInicial extends CI_Controller {
+
+
+	    function __construct() {
+	        parent::__construct();
+	        $this->output->set_header('Access-Control-Allow-Origin: *');
+					$this->load->helper('url');
+	        //$this->load->model('tionmodel');
+	         //$this->load->database();
+	        //Load them in the constructor
+
+
+	    }
+
+
+	public function index()
+	{
+		$this->load->view('pagina_inicial');
+	}
+
+
+
+
+}
---
+++ application/views/pagina_inicial.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+ <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+<link rel="icon" href="/favicon.ico" type="image/gif">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+
+    <title>Galaxia Bar</title>
+
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('application/views/'); ?>css/p_inicial.css">
+</head>
+<body>
+
+<div class="base" >
+    <div class="conteudo" >
+    <div class="texto2">Desenvolvido a pensar em ti.<div>
+        <div class="logo"></div>
+          <br><br><br><br>
+        <div class="botao1 centered" >
+          <a class="text">Entra na Galaxia</a>
+        </div>
+    </div>
+</div>
+</body>
+</html>
